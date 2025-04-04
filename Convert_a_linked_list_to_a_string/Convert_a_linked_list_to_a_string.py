f40ab1996c28db9c8f079c3f48ca9be87a96a4d9
Iryna Svyrydenko <svyrydenko.pn@edu.ucu.ua>
2025-04-04 20:01:39 +0300

    Convert_a_linked_list_to_a_string

--- Convert_a_linked_list_to_a_string/Convert_a_linked_list_to_a_string.py
+++ Convert_a_linked_list_to_a_string/Convert_a_linked_list_to_a_string.py
@@ -6,22 +6,21 @@ class Node:
 def stringify(node):
     if node is None:
         return "None"
-    
+
     result = str(node.data)
-    
+
     current = node.next
     while current is not None:
         result += " -> " + str(current.data)
         current = current.next
-    
+
     result += " -> None"
-    
+
     return result
 
 node = Node(1)
 node.next = Node(2)
 node.next.next = Node(3)
 
-print(stringify(node))  
-print(stringify(None))  
-
+print(stringify(node))
+print(stringify(None))
